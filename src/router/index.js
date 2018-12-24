94bbfaa24c2c92cc63111025d494930bc8672556
ping xie <yurowxie@zjbird.com>
2018-12-24 17:44:59 +0800

    大小写问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
+import Login from '@/components/login'
 import userInfo from '@/components/userInfo'
 import settings from '@/components/settings'
 import userInfoSucc from '@/components/userInfoSucc'
