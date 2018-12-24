cc35b913e2d712a53ac69afabe8e7e17902654fb
ping xie <yurowxie@zjbird.com>
2018-12-24 17:10:02 +0800

    文件路径

--- config/index.js
+++ config/index.js
@@ -12,9 +12,9 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://dist.zjbird.com/',
-    // assetsPublicPath: '/',
-    // assetsPublicPath: public_path,
+    //assetsPublicPath: 'http://dist.zjbird.com/',
+    assetsPublicPath: '/',
+    //assetsPublicPath: public_path,
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
