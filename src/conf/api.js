dc3168d842f121b964fc19ffee7f7da246eb267b
ping xie <yurowxie@zjbird.com>
2018-12-25 11:16:18 +0800

    修正测试环境判定

--- src/conf/api.js
+++ src/conf/api.js
@@ -1,5 +1,9 @@
 
 // var api = 'http://apiv2.e1.fat.zjbird.com';
+//var api = 'http://apiv2.zjbird.com';
 var api = 'http://apiv2.zjbird.com';
+if(window.location.hostname == 'localhost' || window.location.hostname.indexOf('fat')){
+    api = 'http://apiv2.e1.fat.zjbird.com';
+}
 export default api;
 
