6fb1ea420b982c83af9e0c38fa6917c01f088bb4
ping xie <yurowxie@zjbird.com>
2018-12-25 11:26:27 +0800

    修正环境地址

--- src/conf/api.js
+++ src/conf/api.js
@@ -3,7 +3,7 @@
 //var api = 'http://apiv2.zjbird.com';
 var api = 'http://apiv2.zjbird.com';
 if(window.location.hostname == 'localhost' || window.location.hostname.indexOf('fat')){
-    api = 'http://apiv2.e1.fat.zjbird.com';
+    api = 'http://apiv2.e2.fat.zjbird.com';
 }
 export default api;
 
