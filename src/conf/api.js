ae5f6acdb06bbc987c01230694d255be4aa6a9b3
jinghy <857885610@qq.com>
2019-02-13 14:08:40 +0800

    微信扫码付款--api.js 修改api url

--- src/conf/api.js
+++ src/conf/api.js
@@ -1,7 +1,7 @@
 import lstore from '@/tools/lstore';
-// var api = 'http://apiv2.e1.fat.zjbird.com';
+var api = 'http://apiv2.e1.fat.zjbird.com';
 //var api = 'http://apiv2.zjbird.com';
-var api = 'http://apiv2.zjbird.com';
+// var api = 'http://apiv2.zjbird.com';
 lstore.set_item("wx",true);
 if(window.location.hostname == 'localhost' || window.location.hostname.indexOf('fat')){
     api = 'http://apiv2.e2.fat.zjbird.com';
@@ -9,7 +9,6 @@ if(window.location.hostname == 'localhost' || window.location.hostname.indexOf('
 if(window.location.hostname == 'localhost'){
     lstore.set_item("wx",false);
 }
-
+console.log(window.location.hostname)
 export default api;
 
-                                                           
