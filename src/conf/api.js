c30e1020f16fe04a700cc77c99565ac38af273ca
jinghy <857885610@qq.com>
2019-02-13 15:27:10 +0800

    微信扫码付款--api.js 修改api url-------优化

--- src/conf/api.js
+++ src/conf/api.js
@@ -1,13 +1,13 @@
 import lstore from '@/tools/lstore';
-// var api = 'http://apiv2.e1.fat.zjbird.com';
+var api = 'http://apiv2.e1.fat.zjbird.com';
 // var api = 'http://apiv2.zjbird.com';
 var api = '';
 lstore.set_item("wx", true);
-if (window.location.hostname == 'localhost' || window.location.hostname.indexOf('fat') > -1) {
-  api = 'http://apiv2.e2.fat.zjbird.com';
-} else {
-  api = 'http://apiv2.zjbird.com';
-}
+// if (window.location.hostname == 'localhost' || window.location.hostname.indexOf('fat') > -1) {
+//   api = 'http://apiv2.e2.fat.zjbird.com';
+// } else {
+//   api = 'http://apiv2.zjbird.com';
+// }
 if (window.location.hostname == 'localhost') {
   lstore.set_item("wx", false);
 }
